6c85024f1e26c40ae970c85ee1009344f41dd0da
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 21:25:17 +0100

    New Commit

--- src/main/java/fr/diax/ss/command/SsCommand.java
+++ src/main/java/fr/diax/ss/command/SsCommand.java
@@ -17,7 +17,7 @@ import org.bukkit.inventory.meta.SkullMeta;
 
 public class SsCommand implements CommandExecutor, Listener {
 
-    private String prefix = "§6SécurityServer §5> ";
+    private final String prefix = "§6SécurityServer §5> ";
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (sender instanceof Player) {
