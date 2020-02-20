e2471b63d1821bf0f9bb429c5d5753dc867bf390
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 22:03:02 +0100

    New Commit

--- src/main/java/fr/diax/ss/cheattypeEnum/CheatType.java
+++ src/main/java/fr/diax/ss/cheattypeEnum/CheatType.java
@@ -2,5 +2,21 @@ package fr.diax.ss.cheattypeEnum;
 
 public enum CheatType {
 
+    Fly("Fly"),
+    Freecam("Freecam");
+    private String name;
+
+
+    private CheatType type;
+
+    CheatType(String name) {
+        this.name = name;
+
+
+    }
+
+    public CheatType getType() {
+        return type;
+    }
 
 }
